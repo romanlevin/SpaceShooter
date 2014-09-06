3f5b460a3d3f218b1d32eef037cb68606a578976
Roman Levin <romanlevin@gmail.com>
2014-09-06 18:13:15 +0200

    :stars: Randomized asteroid meshes!

---
+++ Assets/Scripts/ChooseRandomMesh.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseRandomMesh : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer> (true);
+        MeshRenderer selectedMeshRenderer;
+        Debug.Log (meshRenderers);
+        selectedMeshRenderer = meshRenderers [Random.Range (0, meshRenderers.Length)];
+        Debug.Log (selectedMeshRenderer);
+        Debug.Log (selectedMeshRenderer.enabled);
+        selectedMeshRenderer.enabled = true;
+        Debug.Log (selectedMeshRenderer.enabled);
+    }
+	
+
+}
